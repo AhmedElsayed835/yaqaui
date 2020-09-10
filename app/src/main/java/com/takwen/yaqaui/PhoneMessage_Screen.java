d2b6da5a64e030cb0029a43d8ab99f28dba20370
Ahmed Elsayed <ahmed_elsayed7071@outlook.com>
2020-09-10 19:27:18 +0200

    Initial Commit

---
+++ app/src/main/java/com/takwen/yaqaui/PhoneMessage_Screen.java
@@ -0,0 +1,14 @@
+package com.takwen.yaqaui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PhoneMessage_Screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phone_message__screen);
+    }
+}
---
+++ app/src/main/java/com/takwen/yaqaui/ShowMainScreenGragment.java
@@ -0,0 +1,14 @@
+package com.takwen.yaqaui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShowMainScreenGragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_main_screen_gragment);
+    }
+}
