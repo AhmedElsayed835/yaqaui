cdc0e083c0c3bea987a9261d3c5e16a3e9517cb6
Ahmed Elsayed <ahmed_elsayed7071@outlook.com>
2020-09-13 11:48:39 +0200

    Pre-Final UI

---
+++ app/src/main/java/com/takwen/yaqaui/FastOrder.java
@@ -0,0 +1,14 @@
+package com.takwen.yaqaui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FastOrder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fast_order);
+    }
+}
---
+++ app/src/main/java/com/takwen/yaqaui/UserOptionsScreen.java
@@ -0,0 +1,14 @@
+package com.takwen.yaqaui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserOptionsScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_options_screen);
+    }
+}
